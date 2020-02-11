7824e7385e34ccafd06cdf6a9afc64cc7f565e8b
Ike Otu <ikeotu@msn.com>
2020-02-10 18:22:18 -0800

    initial commit to establish pull req for daily retro form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import React from 'react';
 
+const myTodos = [
+  {
+    task: 'Study Code',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Go to the gym',
+    id: Date.now(),
+    completed: false
+  },
+  {
+      task: 'Learn to play piano',
+      id: Date.now(),
+      completed: false
+  },
+  {
+      task: 'Play with the kids',
+      id: Date.now(),
+      completed: false
+  }
+];
+
 class App extends React.Component {
+  // React.Component is what we’ll be extending as our base class for class components.
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  //Constructor with state
+  //What we inherit from React.Component:
+  //this.state
+  //this.setState
+  //lifecycle methods
+  constructor() {
+    super(); // we call Super to choose what specifically we want to inherit from React.Component
+    this.state = { // state inside a class component is always going to be an object. 
+      todoData: myTodos 
+    }; 
+  }
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm />
+
+        <TodoList todos={this.state.todoData}/>
       </div>
     );
   }
